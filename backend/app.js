f540805f3d8a37e67ccd82c617f1947a75325c28
Jason Smit <jason.michael.smit@gmail.com>
2021-04-20 16:47:31 +1200

    backend lint dir changed to root. Errors fixed

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,24 @@
 // app.js
 
-const express = require('express');
-const connectDB = require('./config/db');
-var cors = require('cors');
+const express = require("express")
+const cors = require("cors")
+const connectDB = require("./config/db")
 
-const app = express();
+const app = express()
 
 // Connect Database
-connectDB();
+connectDB()
 
 // cors
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: true, credentials: true }))
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }))
 
 // use Routes
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get("/", (req, res) => res.send("Hello world!"))
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 8082
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`))
